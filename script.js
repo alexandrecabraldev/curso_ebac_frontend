2e8d438ed3c6ef653c5ae7c9943a4ab77dc65e1b
alexandrecabraldev <alexandre_c.m@hotmail.com>
2023-09-30 11:36:14 -0300

    add exercise

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-const form = document.querySelector('form');
-const button = document.querySelector('button');
-const input1 = document.querySelector('#number1');
-const input2 = document.querySelector('#number2');
+const form = $('form');
+const ul = $('ul');
+const input = $('input');
 
-form.addEventListener('submit', (event)=>{
+form.on('submit',(event)=>{
     event.preventDefault();
 
-    if(parseInt(input1.value)>=parseInt(input2.value)){
-        alert(`númeor 2 tem que ser maior que o número 1`);
-        reset();
-    }else{
-        alert(`Parabéns, os números estão corretos!!!`);
-        reset();
-    }
+    const inputValue = input.val();
+    ul.append('<li>' + inputValue + '</li>')
+    input.val('');
 })
 
-function reset(){
-    input1.value='';
-    input2.value='';
-}
+ul.on('click','li',function(){
+    $(this).css('text-decoration', 'line-through');
+})
