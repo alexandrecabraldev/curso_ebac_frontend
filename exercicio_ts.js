421524d9d9ca69bd35bc4322d4704bd1e9ff216e
alexandrecabraldev <alexandre_c.m@hotmail.com>
2023-10-11 17:33:36 -0300

    add exercise

---
+++ exercicio_ts.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplierTwoNumbers(a, b) {
+    return a * b;
+}
+function greeting(name) {
+    return "Olà " + name;
+}
+console.log(multiplierTwoNumbers(2, 3));
+console.log(greeting("Jorge"));
---
+++ exercicio_ts.ts
@@ -0,0 +1,12 @@
+function multiplierTwoNumbers(a:number,b:number):number{
+    return a * b;
+}
+
+function greeting(name:string):string{
+
+    return "Olà "+name;
+
+}
+
+console.log(multiplierTwoNumbers(2,3));
+console.log(greeting("Jorge"));
